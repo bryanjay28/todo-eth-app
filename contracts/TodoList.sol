44b1f28bc4726d23aa591f41069a4b25d393e302
bryanjay28 <bryanjay@live.com>
2021-12-08 23:45:57 -0500

    Can create tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,4 +2,34 @@ pragma solidity ^0.5.0;
 
 contract TodoList {
     uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;
+
+    event TaskCreated(
+        uint id,
+        string content,
+        bool completed
+    );
+
+    event TaskToggle();
+
+    constructor() public {
+        createTask("Check out my boy Banks");
+    } 
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+        emit TaskCreated(taskCount, _content, false);
+    }
+
+ //   function toggleCompletion(uint memory _id) public {
+        //taskCount[_id];
+ //   }
 }
